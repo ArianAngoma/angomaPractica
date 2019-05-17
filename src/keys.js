bca50716fac4155aa30a9758a70f23f1fee8dfb1
unknown <arian.angoma@tecsup.edu.pe>
2019-05-16 20:33:58 -0500

    Conexion a RDS_4_

--- src/keys.js
+++ src/keys.js
@@ -2,7 +2,6 @@ module.exports = {
     database: {
         hots: 'angomards.cjlkjff8wmcw.us-east-2.rds.amazonaws.com',
         user: 'angomaRDS',
-        port: '3306',
         password: 'T3csup1838',
         database: 'angomaRDS'
     }
